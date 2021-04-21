93b5c6c51b90cd1a157f36f1dd95f005d6385cfe
Augustin Berisa <burim.berisa@protonmail.com>
2021-04-21 12:39:13 +0200

    Make dialog like warnings

--- src/components/HighImpliedPriceWarning.js
+++ src/components/HighImpliedPriceWarning.js
@@ -1,26 +1,34 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Alert, AlertTitle, AlertDescription, Box, Link, UnorderedList, ListItem } from '@chakra-ui/react'
-import { prettifyCurrency } from '../common/utils'
+import { AlertIcon, Box, Alert, AlertTitle, AlertDescription, Link, CloseButton } from '@chakra-ui/react'
 
 export const HighImpliedPriceWarning = (props) => {
 
 	HighImpliedPriceWarning.propTypes = {
-		vethImpliedPriceUsd: PropTypes.number,
-		vethMarketPriceUsd: PropTypes.number,
+		impliedValue: PropTypes.string,
+		price: PropTypes.string,
+		setState: PropTypes.any.isRequired,
 	}
 
 	return (
-		<Alert status='warning' mb={2}>
+		<Alert
+			status="warning"
+			variant="subtle"
+			flexDirection="row"
+			alignItems="center"
+			justifyContent="center"
+			textAlign="justify"
+		>
+			<AlertIcon />
 			<Box flex='1'>
 				<AlertTitle>Are you sure you want to burn?</AlertTitle>
 				<AlertDescription display='block'>
-					<UnorderedList>
-						<ListItem>Implied price ({prettifyCurrency(props.vethImpliedPriceUsd, 0, 2)}) is already above market ({prettifyCurrency(props.vethMarketPriceUsd, 0, 2)})!</ListItem>
-						<ListItem>Consider <Link isExternal href='https://app.uniswap.org/#/swap/0x4ba6ddd7b89ed838fed25d208d4f644106e34279' style={{ textDecoration: 'underline' }}>buying on Uniswap</Link> today instead.</ListItem>
-					</UnorderedList>
+					Implied price {props.impliedValue ? <i>({props.impliedValue})</i> : ''} is already above market{props.price ? <i> ({props.price})</i> : ''}!
+					<br/>
+					Consider <Link isExternal href='https://app.uniswap.org/#/swap/0x4ba6ddd7b89ed838fed25d208d4f644106e34279' style={{ textDecoration: 'underline' }}>buying on Uniswap</Link> now instead.
 				</AlertDescription>
 			</Box>
+			<CloseButton position="absolute" right="8px" top="8px" onClick={() => props.setState(0)}/>
 		</Alert>
 	)
 }
